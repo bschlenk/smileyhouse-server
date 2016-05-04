0f585437f6fbe00f2c8dd08d55848976c92d7b11
Brian Schlenker <bschlenk@umich.edu>
2016-05-03 23:40:28 -0700

    registration functionality is working

--- main.js
+++ main.js
@@ -1,14 +1,23 @@
 'use strict';
 
-let pkg = require('./package.json')
+const pkg = require('./package.json');
+const Server = require('./lib/server');
+const config = require('config');
+const colors = require('colors');
+const PORT = config.get('port');
 
-let banner = `\
+const banner = `\
 ###################################################
 #                                                 #
 #             Smiley House ${pkg.version}                  #
 #                                                 #
 ###################################################
 
-`
+`.yellow;
 
 console.log(banner);
+
+const server = new Server();
+server.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`);
+});
