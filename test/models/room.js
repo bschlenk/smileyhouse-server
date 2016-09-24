b455258f7fc2b7444715d7c7efa9b04a04936b4d
Brian Schlenker <bschlenk@umich.edu>
2016-09-24 12:57:38 -0700

    Add caching to Room loading
    Rooms now load through static loadRooms method to only load them once.
    This is because they load the rooms through synchronous file loading,
    which could be expensive.
    
    Also added tests for caching using proxyquire and sinon spies.

--- test/models/room.js
+++ test/models/room.js
@@ -1,9 +1,12 @@
 'use strict';
 
 const assert = require('assert');
+const proxyquire = require('proxyquire');
+const sinon = require('sinon');
 const Room = require('../../lib/models/room');
 
 describe('Room', () => {
+
     describe('#fromUser', () => {
         it('should create a Room object from a User', () => {
             const user = {
@@ -29,6 +32,22 @@ describe('Room', () => {
             assert.equal(room.editorVersion(), '107');
 
             assert.equal(room.walls().length, 5);
-        })
+        });
     });
+
+    describe('#loadRoom', () => {
+        const util = require('../../lib/util');
+        const loadJSON = sinon.spy(util, 'loadJSON');
+        const Room = proxyquire('../../lib/models/room', { loadJSON });
+
+        it('should cache the result', () => {
+            const room1 = Room.loadRoom('BunnyDungeon15');
+            const room2 = Room.loadRoom('BunnyDungeon15');
+            const room3 = Room.loadRoom('BunnyDungeon15');
+            const room4 = Room.loadRoom('BunnyDungeon15');
+            const room5 = Room.loadRoom('BunnyDungeon15');
+
+            assert(loadJSON.calledOnce);
+        });
+    })
 });
